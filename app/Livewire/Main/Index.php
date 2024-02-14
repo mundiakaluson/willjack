2014500fa6a106ab6320a229f2debf46308ab955
Jackson Mundia <mundiakaluson@gmail.com>
2024-02-14 10:53:46 +0000

    layout configs

--- app/Livewire/Main/Index.php
+++ app/Livewire/Main/Index.php
@@ -2,10 +2,14 @@
 
 namespace App\Livewire\Main;
 
+use Livewire\Attributes\Layouts;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Layout('main.layouts.app')]
+    #[Title('Willjack System')]
     public function render()
     {
         return view('livewire.main.index');
