256496a7c406ae9b1c800ddddad8ef7475d79448
Jackson Mundia <mundiakaluson@gmail.com>
2024-02-14 09:59:04 +0000

    property configs

--- app/Models/Landlord.php
+++ app/Models/Landlord.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Property;
 
 class Landlord extends Model
 {
     use HasFactory;
+
+    protected $fillable =
+    [
+        'first_name',
+        'last_name',
+        'initials',
+        'pin_number',
+        'vat_number',
+        'postal_address',
+        'email',
+        'phone_number',
+        'alternative_phone_number',
+        'office',
+    ];
+    public function property(): HasMany
+    {
+        return $this->hasMany(Property::class);
+    }
 }
